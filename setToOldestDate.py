d0d4ff6c7786fce6a074292aa3ba7f8239b04a76
Pratyush Tewari <tewari.pratyush@gmail.com>
2018-12-25 03:54:51 -0500

    Completed the script that will change (almost) all the dates of the media to the oldest date

--- setToOldestDate.py
+++ setToOldestDate.py
@@ -1,34 +1,51 @@
+
 # Author Pratyush Tewari
 # Usage `python3 setToOldestDate.py <dir>`
+# This depends upon the oldest_datetime_config without which this script will not work
 
 import os
 import sys
+import subprocess
 from subprocess import Popen, PIPE
 
 absolutepath = ''
+
 fn = sys.argv[1]
 if os.path.exists(fn):
     absolutepath = os.path.abspath(fn)
-    
+else:
+    print ("The path [" + str(fn) + "] does not exists.") 
+    print ("Exiting program!")
+    sys.exit()
 
-def fix_files(absolutepath):
-    # r = []    
-    for root, dirs, files in os.walk(absolutepath):
-        for name in files:
-            b = os.path.join(root, name)
-            # Change all the times to the oldest time
-            output1 = Popen(["exiftool", "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
-            
-            # touch the file to the oldest time
-            output = Popen(["exiftool", "-d", "%Z" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
+total = 0
 
-            print (str(b) + " --> " + output1 + " --> " + output)
-        for name in dirs:
-            # r.append(" ")
-            a = os.path.join(root, name)
-            list_files(a)
-    # return r
-    return
+for dirName, subdirList, fileList in os.walk(absolutepath):
 
-fix_files(absolutepath)
+    for fname in fileList:
+        # ignore all hidden files
+        if fname.startswith(".") :
+            print ("Not Processing " + fname)
+            continue
+        
+        b = os.path.join(dirName, fname)
+        # Change all the times to the oldest time
+        #exiftool -overwrite_original_in_place "-FileModifyDate<OldestDateTime" "-ModifyDate<OldestDateTime" "-DateTimeOriginal<OldestDateTime" "-CreateDate<OldestDateTime" "-GPSDateTime<OldestDateTime" -S -s FILEorDIR
+        command = 'exiftool -overwrite_original_in_place "-FileModifyDate<OldestDateTime" "-ModifyDate<OldestDateTime" "-DateTimeOriginal<OldestDateTime" "-CreateDate<OldestDateTime" "-GPSDateTime<OldestDateTime" -S -s ' + str(b).replace(" ", "\ ")
+        subprocess.Popen([command], stdout=PIPE, universal_newlines=True, shell=True)
+        
+        # get createtime
+        time = Popen(["exiftool", "-d", "%Y%m%d%H%M.%S" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
+        
+        #get Timezone
+        timezone = Popen(["exiftool", "-d", "%Z" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
+        
+        # touch the file to the oldest time
+        subprocess.run(["TZ=" + timezone + " touch -t "+  time + " " + str(b).replace(" ", "\ ")], shell=True, check=True)
+        # global total
+        total += 1
+        # To print on top of last output, add ",end = '\r'" at the end of the print statement
+        print (str(b) + " --> " + " --> " + time + " --> " + timezone )
 
+print ("")
+print ("Total files updated : " + str(total))
