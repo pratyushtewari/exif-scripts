ce0afbb67451d64c8859eefe041fea625bf16cab
Pratyush Tewari <tewari.pratyush@gmail.com>
2018-12-25 02:05:56 -0500

    fixed the quotes around the timezone

--- setToOldestDate.py
+++ setToOldestDate.py
@@ -20,9 +20,9 @@ def fix_files(absolutepath):
             output1 = Popen(["exiftool", "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
             
             # touch the file to the oldest time
-            output = Popen(["exiftool", "-d", "'%Z'" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
+            output = Popen(["exiftool", "-d", "%Z" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
 
-            print (str(b) + " --> " + str(output1) + " --> " + str(output))
+            print (str(b) + " --> " + output1 + " --> " + output)
         for name in dirs:
             # r.append(" ")
             a = os.path.join(root, name)
@@ -31,3 +31,4 @@ def fix_files(absolutepath):
     return
 
 fix_files(absolutepath)
+
