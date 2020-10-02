6fcbee0ad3e51fcc8db7fa3066dc6e2c8b1077ba
Pratyush Tewari <tewari.pratyush@gmail.com>
2020-10-02 01:12:57 -0400

    Added timer to flush the command buffer and added ( ) filename escape charaters

--- setToOldestDate.py
+++ setToOldestDate.py
@@ -31,28 +31,37 @@ for dirName, subdirList, fileList in os.walk(absolutepath):
 
         b = os.path.join(dirName, fname)
 
-        filename = str(b).replace(" ", "\ ").replace("'", "\'")
+        filename = str(b).replace(" ", "\ ").replace("'", "\'").replace("(", "\(").replace(")", "\)")
         # Change all the times to the oldest time
         #exiftool -overwrite_original_in_place "-FileModifyDate<OldestDateTime" "-ModifyDate<OldestDateTime" "-DateTimeOriginal<OldestDateTime" "-CreateDate<OldestDateTime" "-GPSDateTime<OldestDateTime" -S -s FILEorDIR
-        command = 'exiftool -overwrite_original "-FileModifyDate<OldestDateTime" "-ModifyDate<OldestDateTime" "-DateTimeOriginal<OldestDateTime" "-CreateDate<OldestDateTime" "-GPSDateTime<OldestDateTime" -S -m -progress -ee ' + ' ' + filename
+        command = 'exiftool -overwrite_original "-FileModifyDate<OldestDateTime" "-ModifyDate<OldestDateTime" "-DateTimeOriginal<OldestDateTime" "-CreateDate<OldestDateTime" "-GPSDateTime<OldestDateTime" -S -m -progress -ee -q -q ' + filename
         subprocess.Popen([command], stdout=PIPE, universal_newlines=True, shell=True)
-        
+        time.sleep(.5)
+
         # get createtime
-        command = 'exiftool -d "%Y%m%d%H%M.%S" -OldestDateTime -S -s -ee ' +  filename
-        createtime = subprocess.Popen([command], stdout=PIPE, universal_newlines=True, shell=True).communicate()[0].strip()
-        
+        #command = 'exiftool -d "%Y%m%d%H%M.%S" -OldestDateTime -S -s -ee ' +  filename
+        #createtime = subprocess.Popen([command], stdout=PIPE, universal_newlines=True, shell=True).communicate()[0].strip()
+        # time.sleep(.1)
+
         #get Timezone
-        #timezone = Popen(["exiftool", "-d", "%Z" , "-OldestDateTime", "-S", "-s",  str(b)], stdout=PIPE, universal_newlines=True).communicate()[0].strip()
+        #command = 'exiftool -d %Z -OldestDateTime -S -s ' + filename
+        #timezone = Popen([command], stdout=PIPE, universal_newlines=True, shell=True).communicate()[0].strip()
         
         # touch the file to the oldest time
-        #subprocess.run(["TZ=" + timezone + " touch -t "+  time + " " + str(b).replace(" ", "\ ")], shell=True, check=True)
+        #command = "TZ=" + timezone + " touch -t "+  createtime + " " + filename
+        #subprocess.run([command], shell=True, check=True)
+        # time.sleep(.1)
+
         # global total
         total += 1
-        command = 'exiftool "-filename<OldestDateTime" -d %Y%m%d_%H%M%S%%-c.%%e -S -m -ee -progress ' + ' ' + filename
+
+        # Rename the file
+        command = 'exiftool "-filename<OldestDateTime" -d %Y%m%d_%H%M%S%%-c.%%e -S -m -ee -progress -q -q ' + filename
         subprocess.Popen([command], stdout=PIPE, universal_newlines=True, shell=True)
         # To print on top of last output, add ",end = '\r'" at the end of the print statement
-        print (str(b) + " --> " + " --> " + createtime)
-        time.sleep(.1)
+        # print (filename + " --> " + createtime  ,end = '\r');
+        print ("Processing " + str(total) + " of " + str(len(fileList)), end = '\r');
+        time.sleep(.5)
 
 print ("")
 print ("Total files updated : " + str(total))
